aa0d0cb25056db45feafd3ea3d82599c51f63978
Jun-Ki, Hong <dylanhong920509@gmail.com>
2021-01-10 20:48:29 +0900

    build tool upadte

--- CMake/ToolChain/clang/ToolChain.cmake
+++ CMake/ToolChain/clang/ToolChain.cmake
@@ -39,3 +39,20 @@ set(CMAKE_LINKER                "${TOOL}/bin/lld-link${Filename_Extension}")
 # RC compiler
 set(CMAKE_RC_COMPILER           "${TOOL}/bin/llvm-rc${Filename_Extension}")
 
+# DLL tool
+set(CMAKE_DLLTOOL               "${TOOL}/bin/llvm-dlltool${Filename_Extension}")
+
+# NM
+set(CMAKE_NM                    "${TOOL}/bin/llvm-nm${Filename_Extension}")
+
+# OBJDUMP
+set(CMAKE_OBJCOPY               "${TOOL}/bin/llvm-objcopy${Filename_Extension}")
+
+# OBJDUMP
+set(CMAKE_OBJDUMP               "${TOOL}/bin/llvm-objdump${Filename_Extension}")
+
+# RANLIB
+set(CMAKE_RANLIB                "${TOOL}/bin/llvm-ranlib${Filename_Extension}")
+
+# STRIP
+set(CMAKE_STRIP                 "${TOOL}/bin/llvm-strip${Filename_Extension}")
