b7d8bcd13ef3532602030dce6b8b21e17fa287d5
JunKi, Hong <dylanhong920509@gmail.com>
2021-01-10 19:05:35 +0900

    Platfom File Extension

--- CMake/ToolChain/clang/ToolChain.cmake
+++ CMake/ToolChain/clang/ToolChain.cmake
@@ -1,6 +1,7 @@
-#
+# Config Platfom
 if(WIN32)
     set(TOOL "C:/Program\ Files/LLVM")
+    set(Filename_Extension  ".exe")
 elseif(UNIX)
     if(DEFINED LLVM_VERSION)
         set(TOOL "/lib/llvm-${LLVM_VERSION}/")
@@ -12,29 +13,29 @@ else()
 endif()
 
 # LLVM archiver
-set(CMAKE_AR                    "${TOOL}/bin/llvm-lib.exe")
+set(CMAKE_AR                    "${TOOL}/bin/llvm-lib${Filename_Extension}")
 
 # CXX compiler
-set(CMAKE_CXX_COMPILER          "${TOOL}/bin/clang++.exe")
+set(CMAKE_CXX_COMPILER          "${TOOL}/bin/clang++${Filename_Extension}")
 
 # LLVM archiver
-set(CMAKE_CXX_COMPILER_AR       "${TOOL}/bin/llvm-ar.exe")
+set(CMAKE_CXX_COMPILER_AR       "${TOOL}/bin/llvm-ar${Filename_Extension}")
 
 # Generate index for LLVM archive
-set(CMAKE_CXX_COMPILER_RANLIB   "${TOOL}/bin/llvm-ranlib.exe")
+set(CMAKE_CXX_COMPILER_RANLIB   "${TOOL}/bin/llvm-ranlib${Filename_Extension}")
 
 # C compiler
-set(CMAKE_C_COMPILER            "${TOOL}/bin/clang.exe")
+set(CMAKE_C_COMPILER            "${TOOL}/bin/clang${Filename_Extension}")
 
 # LLVM archiver
-set(CMAKE_C_COMPILER_AR         "${TOOL}/bin/llvm-ar.exe")
+set(CMAKE_C_COMPILER_AR         "${TOOL}/bin/llvm-ar${Filename_Extension}")
 
 # Generate index for LLVM archive
-set(CMAKE_C_COMPILER_RANLIB     "${TOOL}/bin/llvm-ranlib.exe")
+set(CMAKE_C_COMPILER_RANLIB     "${TOOL}/bin/llvm-ranlib${Filename_Extension}")
 
 # Linker
-set(CMAKE_LINKER                "${TOOL}/bin/lld-link.exe")
+set(CMAKE_LINKER                "${TOOL}/bin/lld-link${Filename_Extension}")
 
 # RC compiler
-set(CMAKE_RC_COMPILER           "${TOOL}/bin/llvm-rc.exe")
+set(CMAKE_RC_COMPILER           "${TOOL}/bin/llvm-rc${Filename_Extension}")
 
