74efb804d937f2e80b11a5a0994318d1e572abe2
JunKi, Hong <dylanhong920509@gmail.com>
2021-06-09 00:18:29 +0900

    separate the os platform

--- Template/.props/config.mk
+++ Template/.props/config.mk
@@ -2,40 +2,44 @@
 # Set Configurations
 #============================================================
 # File Extension
-FILE_EXT		:=	elf
+ifeq ($(OS),Windows_NT)
+	FILE_EXT		:=	exe
+else
+	FILE_EXT		:=	elf
+endif
 
 # Build Mode( debug | release )
-BUILD_MODE		:=	debug
+BUILD_MODE			:=	debug
 
 # Display Build Log( 0 | 1 )
-BUILD_VERBOSE	:=	0
+BUILD_VERBOSE		:=	0
 
 # C Standard Version
-C_STANDARD_VER		:= 	99
-CXX_STANDARD_VER	:= 	17
+C_STANDARD_VER		:= 	gnu89
+CXX_STANDARD_VER	:= 	c++17
 
 # Instruction Set(32 | 64)
-INSTRUCTION_SET	:=	64
+INSTRUCTION_SET		:=	64
 
 #============================================================
 # Set Compile & Link Options by Configurations
 #============================================================
-CFLAGS			:= -m$(INSTRUCTION_SET) -std=c$(C_STANDARD_VER) -Wall -pedantic
-CXXFLAGS		:= -m$(INSTRUCTION_SET) -std=c++$(CXX_STANDARD_VER) -Wall -pedantic
-LDFLAGS			:= -static
+CFLAGS				:= -m$(INSTRUCTION_SET) -std=$(C_STANDARD_VER) -W -Wall -pedantic-errors
+CXXFLAGS			:= -m$(INSTRUCTION_SET) -std=$(CXX_STANDARD_VER) -W -Wall -pedantic-errors
+LDFLAGS				:= -static
 
 ifeq ($(BUILD_MODE), debug)
-	CFLAGS		+= -g
-	CXXFLAGS	+= -g
-	LDFLAGS		+= -g
+	CFLAGS			+= -g
+	CXXFLAGS		+= -g
+	LDFLAGS			+= -g
 else ifeq ($(BUILD_MODE), release)
-	CFLAGS		+= -O2
-	CXXFLAGS	+= -O2
-	LDFLAGS		+= -O2
+	CFLAGS			+= -O2
+	CXXFLAGS		+= -O2
+	LDFLAGS			+= -O2
 else
 	#error "Invalid BUILD_MODE"
 endif
 
 ifeq ($(BUILD_VERBOSE), 0)
-	V			:=	@
+	V				:=	@
 endif
