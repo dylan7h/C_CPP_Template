0eae5d4291648e241b41071e42e47f4ae9d5eb3b
JunKi, Hong <dylanhong920509@gmail.com>
2021-06-20 02:13:12 +0900

    Autoconf: Basic Operatopn is done

--- Template/.props/autoconf.mk
+++ Template/.props/autoconf.mk
@@ -14,24 +14,32 @@ endef
 #============================================================
 make_autoconf: mkdir
 # Check if the file exists
-	@IS_EXIST_DEF_FILE=$$(test -s $(DEF_DIR)/$(TARGET).def && echo -n y); 	\
-	if [ "$${IS_EXIST_DEF_FILE}" != "y" ]; 									\
-	then 																	\
-		echo "$(DEFINES)" > $(DEF_DIR)/$(TARGET).def ; 						\
-		IS_INVALIDATE_DEF=y ; 												\
-	else 																	\
-		DEFINES_FROM_FILE=$$(cat $(DEF_DIR)/$(TARGET).def) ; 				\
-		if [ "$${DEFINES_FROM_FILE}" != "$(DEFINES)" ]; 					\
-		then 																\
-			IS_INVALIDATE_DEF=y ; 											\
-		fi; 																\
-	fi; 																	\
-																			\
-	if [ "$${IS_INVALIDATE_DEF}" = "y" ]; 									\
-	then 																	\
-		mkdir -p $(AUTO_CONF_DIR) ;											\
-		rm -f $(AUTO_CONF_DIR)/autoconf.h;									\
-		echo "#ifndef __AUTO_CONF_H__" > $(AUTO_CONF_DIR)/autoconf.h ;		\
-		echo "#define __AUTO_CONF_H__" >> $(AUTO_CONF_DIR)/autoconf.h ;		\
-		echo "#endif /* !__AUTO_CONF_H__ */" >> $(AUTO_CONF_DIR)/autoconf.h ;	\
+	@IS_EXIST_DEF_FILE=$$(test -s $(DEF_DIR)/$(TARGET).def && echo -n y); 						\
+	if [ "$${IS_EXIST_DEF_FILE}" != "y" ]; 														\
+	then 																						\
+		echo "$(DEFINES)" > $(DEF_DIR)/$(TARGET).def ; 											\
+		IS_INVALIDATE_DEF=y ; 																	\
+	else 																						\
+		DEFINES_FROM_FILE=$$(cat $(DEF_DIR)/$(TARGET).def) ; 									\
+		if [ "$${DEFINES_FROM_FILE}" != "$(DEFINES)" ]; 										\
+		then 																					\
+			IS_INVALIDATE_DEF=y ; 																\
+		fi; 																					\
+	fi; 																						\
+																								\
+	if [ "$${IS_INVALIDATE_DEF}" = "y" ]; 														\
+	then 																						\
+		mkdir -p $(AUTO_CONF_DIR) ;																\
+		rm -f $(AUTO_CONF_DIR)/autoconf.h;														\
+		echo "#ifndef __AUTO_CONF_H__" > $(AUTO_CONF_DIR)/autoconf.h ;							\
+		echo "#define __AUTO_CONF_H__\n" >> $(AUTO_CONF_DIR)/autoconf.h ;						\
+		for ITEM in $(DEFINES) ; 																\
+		do 																						\
+			DEFINE_SYMBOL=$${ITEM%%=*} ; 														\
+			DEFINE_VALUE=$${ITEM%%*=} ; 														\
+			echo "#ifndef $${DEFINE_SYMBOL}" >> $(AUTO_CONF_DIR)/autoconf.h ;					\
+			echo "#define $${DEFINE_SYMBOL} $${DEFINE_VALUE}" >> $(AUTO_CONF_DIR)/autoconf.h ;	\
+			echo "#endif /* !$${DEFINE_SYMBOL} */\n" >> $(AUTO_CONF_DIR)/autoconf.h ;			\
+		done; 																					\
+		echo "#endif /* !__AUTO_CONF_H__ */" >> $(AUTO_CONF_DIR)/autoconf.h ;					\
 	fi
---
+++ Template/include/autoconf.h
@@ -0,0 +1,24 @@
+#ifndef __AUTO_CONF_H__
+#define __AUTO_CONF_H__
+
+#ifndef DEBUG
+#define DEBUG 
+#endif /* !DEBUG */
+
+#ifndef _DEBUG
+#define _DEBUG 
+#endif /* !_DEBUG */
+
+#ifndef VERSION
+#define VERSION VERSION=10
+#endif /* !VERSION */
+
+#ifndef EVT
+#define EVT EVT=1
+#endif /* !EVT */
+
+#ifndef STD
+#define STD STD=99
+#endif /* !STD */
+
+#endif /* !__AUTO_CONF_H__ */
